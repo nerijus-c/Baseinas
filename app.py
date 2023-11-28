f806ab029647d4c3487c366b4d7d70b7c3ac09be
nerijus-c <149934123+nerijus-c@users.noreply.github.com>
2023-11-28 14:11:08 +0200

    update + new data

--- app.py
+++ app.py
@@ -2,43 +2,46 @@
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
+from uzimtumo_duomenu_analize import baseinas_clean
 
-# Load the dataset
-avocado = pd.read_csv('Baseino_laisvos_vietos_cleen.csv')
-# fig = px.line(avocado, x="Hour", y="Laisvos_vietos")
+### update the newest dataset
+baseinas_clean()
+
+### Load the dataset
+df_uzimtumas = pd.read_csv('Baseino_laisvos_vietos_cleen.csv')
+
+### sort dropdown data list by weekdays
+cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+df_uzimtumas['Weekday'] = pd.Categorical(df_uzimtumas['Weekday'], categories=cats, ordered=True)
+df_uzimtumas = df_uzimtumas.sort_values('Weekday')
+
 # Create the Dash app
 app = Dash()
 server = app.server
 
 
-cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 # Set up the app layout
-geo_dropdown = dcc.Dropdown(options=avocado['Weekday'].unique(),
-                            value='Monday')
+day_dropdown = dcc.Dropdown(options=df_uzimtumas['Weekday'].unique(), value='Monday')
 
 
 app.layout = html.Div(children=[
     html.H1(children='Baseino uzimtumo statistika'),
-    geo_dropdown,
-    dcc.Graph(
-        id='bar_chart',
-        # figure=fig
-    )
+    day_dropdown,
+    dcc.Graph(id='bar_chart')
 ])
 
-# avocado['Laisvos_vietos'] = avocado['Laisvos_vietos'].mean().round(1)
-# print(avocado)
 
 # Set up the callback function
 @app.callback(
     Output(component_id='bar_chart', component_property='figure'),
-    Input(component_id=geo_dropdown, component_property='value')
+    Input(component_id=day_dropdown, component_property='value')
 )
-def update_graph(selected_geography):
-    filtered_avocado = avocado[avocado['Weekday'] == selected_geography].groupby('Hour', as_index=False).mean('Laisvos_vietos').round()
-    line_fig = px.line(filtered_avocado,
+def update_graph(selected_day):
+    filtered_uzimtumas = df_uzimtumas[df_uzimtumas['Weekday'] == selected_day].groupby('Hour', as_index=False).\
+        mean('Laisvos_vietos').round()
+    line_fig = px.line(filtered_uzimtumas,
                        x='Hour', y='Laisvos_vietos',
-                       title=f' {selected_geography}')
+                       title=f' {selected_day}')
     return line_fig
 
 
