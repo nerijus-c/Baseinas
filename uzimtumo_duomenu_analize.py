66af842c60d72b477c95cb25b32e6dd9c5962ad4
nerijus-c <149934123+nerijus-c@users.noreply.github.com>
2023-11-28 14:41:57 +0200

    new csv

--- uzimtumo_duomenu_analize.py
+++ uzimtumo_duomenu_analize.py
@@ -15,7 +15,7 @@ def baseinas_clean():
     # baseinas_cleen_df['Weekday'] = pd.Categorical(baseinas_cleen_df['Weekday'], categories=cats, ordered=True)
     # baseinas_cleen_df = baseinas_cleen_df.sort_values('Weekday')
 
-    baseinas_cleen_df.to_csv('C:/Users/Vartotojas/PycharmProjects/Baseinas/Baseino_laisvos_vietos_cleen.csv',index=False)
+    baseinas_cleen_df.to_csv('Baseino_laisvos_vietos_cleen.csv',index=False)
     print(f'Baseino_laisvos_vietos_cleen: issaugota')
     # print(baseinas_cleen_df)
 
